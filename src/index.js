46dd916e04b20d83ac91848d8fa4eaa2ff5813f2
Guilherme Almeida <guiallan.al@gmail.com>
2020-08-24 00:38:34 -0300

    Entendimento das diferenças entre ReactJS e React-native
    Aula #0022
    
    Ref: NIVEL1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-  return <View />
+  return (
+    <>
+    <StatusBar 
+      barStyle="light-content"
+      backgroundColor="#715999"
+    />
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Go Rocket!</Text>
+    </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    color: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  textTitle: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+})
